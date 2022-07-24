927cbcba44906889809d2e6cf2ca3fd655db6bb8
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-07-24 17:09:15 -0600

    Added product entity to the domain

---
+++ Domain/Products/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Products.Entities
+{
+    public class Product
+    {
+        public int Stock { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Type { get; set; }
+
+        public Product(int stock, string name, double price, string type)
+        {
+            Stock = stock;
+            Name = name;
+            Price = price;
+            Type = type;
+        }
+    }
+}
