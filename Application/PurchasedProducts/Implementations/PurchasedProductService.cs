e438af5b7089bc5b01cd7cc6b4e1d3a667128d06
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-07-25 16:05:37 -0600

    Added Purchased Product Service to the application layer

---
+++ Application/PurchasedProducts/Implementations/PurchasedProductService.cs
@@ -0,0 +1,24 @@
+﻿using Domain.PurchasedProducts.Entities;
+using Domain.PurchasedProducts.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.PurchasedProducts.Implementations
+{
+    public class PurchasedProductService : IPurchasedProductsService
+    {
+        private readonly IPurchasedProductRepository _purchasedProductRepository;
+        public PurchasedProductService(IPurchasedProductRepository purchasedProductRepository)
+        {
+            _purchasedProductRepository = purchasedProductRepository;
+        }
+
+        public void AddPurchasedProduct(PurchasedProduct productPurchased)
+        {
+            _purchasedProductRepository.AddPurchasedProduct(productPurchased);
+        }
+    }
+}
