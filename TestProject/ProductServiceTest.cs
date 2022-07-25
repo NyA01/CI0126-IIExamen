e8bf2e7ceea774ab916a53c89755d8a4af247bc7
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-07-25 11:47:17 -0600

    Added Project for UnitTest

---
+++ TestProject/ProductServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+namespace TestProject
+{
+    public class ProductServiceTest
+    {
+
+        [Fact]
+        public void GetAllProducts()
+        {
+            //arrange
+            //var mockProductRepository = new Mock<IProductRepository>();
+
+
+
+        }
+
+
+
+
+
+
+    }
+}
