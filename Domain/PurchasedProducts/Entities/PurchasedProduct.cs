2a63abfd1adb6c8641823578ce6d18c3f1a4bf5c
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-07-25 15:55:16 -0600

    Added purchased product service and repository to the domain layer

---
+++ Domain/PurchasedProducts/Entities/PurchasedProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.PurchasedProducts.Entities
+{
+    public class PurchasedProduct
+    {
+        public string Name { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+        public string Type { get; set; }
+
+        public PurchasedProduct(string name, int amount, double price, string type)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+            Type = type;
+        }
+    }
+}
