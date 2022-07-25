a72eb06d70d6dea4b1ffa1586bfbbd17a4840bb0
Nayeri Azofeifa <NAYERI.AZOFEIFA@ucr.ac.cr>
2022-07-25 16:19:51 -0600

    Added purchased product DBContext, repository and dependency injection to the infraestructure layer

---
+++ Infrastructure/PurchasedProducts/PurchasedProductDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.PurchasedProducts.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.PurchasedProducts
+{
+    public class PurchasedProductDBContext
+    {
+        public PurchasedProductDBContext() { }
+        public IList<PurchasedProduct> PurchasedProducts = new List<PurchasedProduct>()
+        {
+         
+        };
+    }
+}
---
+++ Infrastructure/PurchasedProducts/Repositories/PurchasedProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Core.Repositories;
+using Domain.PurchasedProducts.Entities;
+using Domain.Products;
+using Domain.PurchasedProducts.Repositories;
+using Infrastructure.PurchasedProducts;
+
+namespace Infrastructure.Products.Repositories
+{
+    public class PurchasedProductRepository : IPurchasedProductRepository
+    {
+        private readonly PurchasedProductDBContext _dbContext;
+
+        public PurchasedProductRepository()
+        {
+            _dbContext = new PurchasedProductDBContext();
+        }
+
+        public void AddPurchasedProduct(PurchasedProduct productPurchased)
+        {
+            _dbContext.PurchasedProducts.Add(productPurchased);
+        }
+    }
+}
